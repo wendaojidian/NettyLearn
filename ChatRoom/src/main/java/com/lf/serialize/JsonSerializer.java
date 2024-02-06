f6cdd8f6187373a7d7d1fccf02c5b01bcd95ee21
liufan58 <liufan58@jd.com>
2024-02-06 16:35:06 +0800

    add spring

--- ChatRoom/src/main/java/com/lf/serialize/JsonSerializer.java
+++ ChatRoom/src/main/java/com/lf/serialize/JsonSerializer.java
@@ -1,12 +1,14 @@
 package com.lf.serialize;
 
 import com.alibaba.fastjson.JSON;
+import org.springframework.stereotype.Service;
 
 /**
  * @author liufan
  * @description: Json序列化
  * @since 2024/02/06
  */
+@Service
 public class JsonSerializer implements Serializer{
     @Override
     public Byte getAlgorithm() {
